60324d4c1e14a3c089ef2bb5f7da701e7a877372
Rexyz <rex@qq.com>
2025-06-03 11:08:04 +0800

    将傅里叶变换后的复数频域矩阵变换后以灰度图的形式展示出来

--- src/widget.cpp
+++ src/widget.cpp
@@ -21,10 +21,10 @@ Widget::Widget(QWidget *parent) : QWidget(parent), ui(new Ui::Widget)
     // }
 
 
-    // cv::Mat xnm = (cv::Mat_<uchar>(4, 4) << 1, 1, 1, 1,
-    //                                         1, 1, 1, 1,
-    //                                         1, 1, 1, 1,
-    //                                         1, 1, 1, 1);
+    // cv::Mat xnm = (cv::Mat_<uchar>(4, 4) << 254, 255, 255, 255,
+    //                                         255, 254, 255, 255,
+    //                                         255, 255, 254, 255,
+    //                                         255, 255, 255, 253);
 
     // cv::Mat Xk0 = FFT2D(xnm, "uchar");
     // for(int i=0; i<Xk0.size[0]; i++)
@@ -52,13 +52,11 @@ void Widget::on_enter_ok_clicked()
         cv::Mat image = cv::imread(file_path.toStdString());
         cv::Mat gray_image;
         cv::cvtColor(image, gray_image, cv::COLOR_BGR2GRAY);
-        // QImage gray_image_toshow(gray_image.data, gray_image.cols, gray_image.rows, gray_image.step, QImage::Format_Grayscale8);
-        // QPixmap pixmap = QPixmap::fromImage(gray_image_toshow);
-        // ui->raw_image->setPixmap(pixmap);
+        QImage gray_image_toshow(gray_image.data, gray_image.cols, gray_image.rows, gray_image.step, QImage::Format_Grayscale8);
+        QPixmap pixmap = QPixmap::fromImage(gray_image_toshow);
+        ui->raw_image->setPixmap(pixmap);
 
         cv::Mat Xkv = FFT2D(gray_image, "uchar");
-
-        std::cout << "Xkv=" << Xkv.at<std::complex<double>>(0, 0) << std::endl;
         cv::Mat Xkv_abs = cv::Mat_<double>(Xkv.size());
         for(int i=0; i<Xkv.size[0]; i++)
         {
@@ -67,17 +65,12 @@ void Widget::on_enter_ok_clicked()
                 Xkv_abs.at<double>(i, j) = std::abs(Xkv.at<std::complex<double>>(i, j));
             }
         }
-        std::cout << "Xkv_abs=" << Xkv_abs.at<double>(0, 0) << std::endl;
-
-        double minVal, maxVal;
-        cv::minMaxLoc(Xkv_abs, &minVal, &maxVal); 
-        std::cout << "minVal=" << minVal << ",maxVal=" << maxVal << std::endl;
 
         cv::Mat Xkv_8u;
         cv::convertScaleAbs(Xkv_abs, Xkv_8u);
         QImage Xkv_8u_toshow(Xkv_8u.data, Xkv_8u.cols, Xkv_8u.rows, Xkv_8u.step, QImage::Format_Grayscale8);
         QPixmap Xkv_8u_pixmap = QPixmap::fromImage(Xkv_8u_toshow);
-        ui->raw_image->setPixmap(Xkv_8u_pixmap);
+        ui->fft_image->setPixmap(Xkv_8u_pixmap);
     }
     else
     {
